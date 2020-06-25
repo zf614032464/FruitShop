117115c6f0fd3128dca587a8c1152aeaef459025
zf614032464 <614032464@qq.com>
2020-06-26 00:40:40 +0800

    更新DBUtils类

--- FruitShop/src/utils/DBUtils.java
+++ FruitShop/src/utils/DBUtils.java
@@ -1,5 +1,29 @@
 package utils;
 
-public class DBUtils {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
+public class DBUtils {
+	public static final String url = "jdbc:mysql://47.93.213.168/fruitshop?characterEncoding=utf8";  
+    public static final String name = "com.mysql.jdbc.Driver";  
+    public static final String user = "root";  
+    public static final String password = "root";  
+ 
+    public Connection conn = null;  
+    public PreparedStatement pst = null;
+    ResultSet result = null;// 创建一个结果集对象
+ 
+    public DBUtils() {  
+        try {  
+            Class.forName(name);//指定连接类型  
+            conn = DriverManager.getConnection(url,user,password);//获取连接  
+            //System.out.println("数据库连接成功"); 
+            
+        } catch (Exception e) {  
+            e.printStackTrace();
+            System.out.println("数据库连接失败");
+        }  
+    }  
 }
